0361123894396323a04bb4a2c3ed1be3f53ec805
Lachlan Burndred <toomenvy@gmail.com>
2016-05-09 19:10:56 +1000

    Changes & added files.
    Extended functionality of the player inventory script, and added a first
    person view camera script.

---
+++ PlayerInventory.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // List of items in player inventory.
+    public List<GameObject> inventoryList;
+
+    // List of text objects.
+    public Text[] inventorySlots;
+
+    // Display text for inventory items.
+    public Text invSlot1;
+    public Text invSlot2;
+    public Text invSlot3;
+    public Text invSlot4;
+    public Text invSlot5;
+
+    // Canvas which displays items in player inventory.
+    public GameObject InventoryCanvas;
+
+    // bool controlling inventory open/close state.
+    public bool invOpen = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add text to list.
+        inventorySlots[0] = invSlot1;
+        inventorySlots[1] = invSlot2;
+        inventorySlots[2] = invSlot3;
+        inventorySlots[3] = invSlot4;
+        inventorySlots[4] = invSlot5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (!invOpen)
+            {
+                OpenInventory();
+            }
+            else
+            {
+                CloseInventory();
+            }
+        }
+
+        for (int i = 0; i < inventoryList.Count; i++)
+        {
+            inventorySlots[i].text = inventoryList[i].name;
+        }
+    }
+
+    void addToInventory(GameObject _item)
+    {
+        inventoryList.Add(_item);
+    }
+
+    void OpenInventory()
+    {
+        invOpen = true;
+        InventoryCanvas.SetActive(true);
+    }
+
+    void CloseInventory()
+    {
+        invOpen = false;
+        InventoryCanvas.SetActive(false);
+    }
+}
